1b9fe98e557f79e5919b3302088c6b735ac08977
GamesProSeif <seifmansour2014@outlook.com>
2022-12-22 21:34:50 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SQLite;
+
+namespace ATM_GUI
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            InitiateDB();
+            Application.Run(new MainForm());
+        }
+
+        static void InitiateDB()
+        {
+            if (!System.IO.File.Exists("database.db3"))
+            {
+                var db = new SQLiteConnection("database.db3");
+                db.CreateTable<User>();
+                db.Close();
+                Console.WriteLine("Created DB");
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ATM_GUI
+{
+    internal class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        private double balance;
+        public double Balance {
+            get { return balance; }
+            set { if (value >= 0) balance = value; }
+        }
+
+        public User() { }
+        public User(string name, string password)
+        {
+            this.Name = name;
+            this.Password = password;
+        }
+    }
+}
